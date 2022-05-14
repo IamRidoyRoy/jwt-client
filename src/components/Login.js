c8430f7fd5b96e39dbf2359662d243ba5aa926c5
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-14 12:07:02 +0600

    Add token verification with node server

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
-
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
+    const navigate = useNavigate();
+    const handleFormValue = event => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const pass = event.target.pass.value;
+        console.log(email, pass);
+
+        fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ email, pass })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    localStorage.setItem('accessToken', data.accessToken);
+                    navigate('/orders')
+                }
+                console.log(data);
+            })
+    }
     return (
         <div className='container w-50'>
-            <Form>
+            <Form onSubmit={handleFormValue}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name='pass' placeholder="Password" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 const Orders = () => {
+    const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        fetch('http://localhost:5000/orders', {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    navigate('/login');
+                }
+                return res.json()
+            })
+            .then(data => {
+                setOrders(data);
+                console.log(data);
+            })
+    }, [])
     return (
         <div>
-            <h2>This is from orders!</h2>
+            <h2>This is from orders! : {orders.length}</h2>
         </div>
     );
 };
